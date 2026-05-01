b7731385c41a58154c81ea4890a18bed0d18d6c4
jorgeav527 <jorgeav527@gmail.com>
2026-04-30 22:00:38 -0500

    feix deb

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/index") # controlador
+@app.route("/") # controlador
 def hello():
     base_de_datos = ["jorge", "pepe", "juanito"] # Modelos
     return render_template(template_name_or_list="index.html", datos=base_de_datos ) # view
