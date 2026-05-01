86fba5d53fa2191725638a8a7233d594b2b7d6e8
jorgeav527 <jorgeav527@gmail.com>
2026-04-30 21:57:34 -0500

    feat: primera funcion

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/index") # controlador
 def hello():
-    return "Hello, World! Hola chicos"
+    base_de_datos = ["jorge", "pepe", "juanito"] # Modelos
+    return render_template(template_name_or_list="index.html", datos=base_de_datos ) # view
