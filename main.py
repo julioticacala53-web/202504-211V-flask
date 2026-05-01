422c039fd1aef48c932738ffa7ff9a31b7171766
jorgeav527 <jorgeav527@gmail.com>
2026-04-30 22:15:39 -0500

    feat: root, home, index

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/") # controlador
-def hello():
+def root():
+    return render_template("base.html" ) # view
+
+@app.route("/index") # controlador
+def index():
     base_de_datos = ["jorge", "pepe", "juanito"] # Modelos
-    return render_template(template_name_or_list="index.html", datos=base_de_datos ) # view
+    return render_template("index.html", datos=base_de_datos ) # view
+
+@app.route("/home") # controlador
+def home():
+    return render_template("home.html") # view
