45a06f5addae26eb6ade9890c2a005dbe9f15bec
jorgeav527 <jorgeav527@gmail.com>
2026-05-07 20:42:28 -0500

    feat: delete, delete htmx

--- main.py
+++ main.py
@@ -75,3 +75,28 @@ def update_post(post_id):
         )
         connection.commit()
         return redirect(url_for("get_all_posts"))
+
+
+@app.route("/post/delete/<int:post_id>", methods=["POST"])
+def delete_one_post(post_id):
+    connection = get_db_connection()
+    cur = connection.cursor()
+    post = cur.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    if post is None:
+        abort(404)
+    connection.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+    connection.commit()
+    return redirect(url_for("get_all_posts"))
+
+
+@app.route("/post/delete/<int:post_id>/htmx", methods=["DELETE"])
+def delete_one_post_htmx(post_id):
+    connection = get_db_connection()
+    cur = connection.cursor()
+    post = cur.execute("SELECT * FROM posts WHERE id = ?", (post_id,)).fetchone()
+    if post is None:
+        abort(404)
+    connection.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+    connection.commit()
+    if request.method == "DELETE":
+        return ""
